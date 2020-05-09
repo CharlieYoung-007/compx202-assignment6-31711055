3da91f8ea01e2a735a9f281c3d5ac70a880c7701
“CharlieYoung-007” <1172373115@qq.com>
2020-05-09 19:43:38 +0800

    the completion of partA

--- app/src/main/java/com/example/assignment6/MainActivity.java
+++ app/src/main/java/com/example/assignment6/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.assignment6;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickButtonStart(View v){
+//        Toast.makeText(this, "message", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
     }
 }
