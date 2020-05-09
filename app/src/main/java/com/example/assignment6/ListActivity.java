4a3ba70f31389a19367e2d30a6436d2a62ea9aec
“CharlieYoung-007” <1172373115@qq.com>
2020-05-09 21:02:36 +0800

    the completion of partB

--- app/src/main/java/com/example/assignment6/ListActivity.java
+++ app/src/main/java/com/example/assignment6/ListActivity.java
@@ -1,13 +1,35 @@
 package com.example.assignment6;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        String[] array = getResources().getStringArray(R.array.listArray);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(messageClickedHandler);
     }
+
+    // Create a message handling object as an anonymous class.
+    private AdapterView.OnItemClickListener messageClickedHandler = new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView parent, View v, int position, long id) {
+            Intent intent = new Intent(ListActivity.this, DetailActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
 }
