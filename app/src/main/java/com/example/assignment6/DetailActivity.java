77f1398343fdcf7a627aaa56be78ab72c764e954
“CharlieYoung-007” <1172373115@qq.com>
2020-05-12 09:45:23 +0800

    amend the partC

--- app/src/main/java/com/example/assignment6/DetailActivity.java
+++ app/src/main/java/com/example/assignment6/DetailActivity.java
@@ -11,46 +11,36 @@ import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
-    String context;
+    String context; //store the detail information to share
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Intent intent = getIntent();
-        String title = intent.getStringExtra("title");
+        String title = intent.getStringExtra("title"); //get the title name
         TextView textView = (TextView) findViewById(R.id.textviewDetailTitle);
         textView.setText(title);
-        ImageView imageView = (ImageView) findViewById(R.id.imageviewDetailImage);
-        int drawableId; //the Variable to store pictures
-        String[] array;
-        TextView detailView = (TextView) findViewById(R.id.textviewDetailContent);
 
-        if (title.equals("Bachelor of Computer Graphic Design")) {
-            drawableId = getResources().getIdentifier("bcgd", "drawable", getPackageName());
-            imageView.setImageResource(drawableId);
-            array = getResources().getStringArray(R.array.Bachelor_of_Computer_Graphic_Design);
-            detailView.setText(array[2]);
-            context = array[2];
-        } else if (title.equals("Bachelor of Media and Creative Technologies")) {
-            drawableId = getResources().getIdentifier("bmct", "drawable", getPackageName());
-            imageView.setImageResource(drawableId);
-            array = getResources().getStringArray(R.array.Bachelor_of_Media_and_Creative_Technologies);
-            detailView.setText(array[2]);
-            context = array[2];
-        } else if (title.equals("Bachelor of Business Analysis")) {
-            drawableId = getResources().getIdentifier("bba", "drawable", getPackageName());
-            imageView.setImageResource(drawableId);
-            array = getResources().getStringArray(R.array.Bachelor_of_Business_Analysis);
-            detailView.setText(array[2]);
-            context = array[2];
-        } else if (title.equals("English Foundation Program")) {
-            drawableId = getResources().getIdentifier("efp", "drawable", getPackageName());
-            imageView.setImageResource(drawableId);
-            array = getResources().getStringArray(R.array.English_Foundation_Program);
-            detailView.setText(array[2]);
-            context = array[2];
-        }
+        int drawableId; //the Variable to store pictures
+        String titleArray;
+        String imageArray;
+
+
+        titleArray = title.replace(" ", "_"); //replace the space with '_'
+
+        //get the detail text
+        int detail_id = getResources().getIdentifier(titleArray,"array",this.getPackageName());
+        String[] detailArray = getResources().getStringArray(detail_id);
+        TextView detailView = (TextView)findViewById(R.id.textviewDetailContent);
+        detailView.setText(detailArray[2]);
+        context = detailArray[2];
+
+        //get the image
+        imageArray = detailArray[0];
+        ImageView imageView = (ImageView) findViewById(R.id.imageviewDetailImage);
+        drawableId = getResources().getIdentifier(imageArray, "drawable", getPackageName());
+        imageView.setImageResource(drawableId);
 
     }
 
